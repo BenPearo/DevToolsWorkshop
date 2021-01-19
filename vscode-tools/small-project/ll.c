0982fbddf9265f0fd5a90972a5d3fa0dabc9cf48
BenPearo <benpearo@gmail.com>
2021-01-18 22:25:59 -0500

    style

--- vscode-tools/small-project/ll.c
+++ vscode-tools/small-project/ll.c
@@ -9,26 +9,26 @@ struct node_struct
   struct node_struct *next;
 };
 
-int main() {
+int main()
+{
+  struct node_struct *node1, *node2;
+  char *data, *string;
 
-    struct node_struct *node1, *node2;
-    char *data, *string;
+  data = malloc(14);
+  data = "This is data!";
 
-    data = malloc(14);
-    data = "This is data!";
+  string = malloc(18);
+  string = "This is a string!";
 
-    string = malloc(18);
-    string = "This is a string!";
+  node1 = malloc(sizeof(struct node_struct));
+  node1->void_data = data;
+  node1->str = string;
 
-    node1 = malloc(sizeof(struct node_struct));
-    node1->void_data = data;
-    node1->str = string;
+  node2 = malloc(sizeof(struct node_struct));
+  node2->void_data = data;
+  node2->str = string;
 
-    node2 = malloc(sizeof(struct node_struct));
-    node2->void_data = data;
-    node2->str = string;
+  node1->next = node2;
 
-    node1->next = node2;
-
-    return 0;
+  return 0;
 }
